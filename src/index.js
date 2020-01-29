40a0a5bfb3e2142159802151fbb3d3439ab243d8
Majid Jaidi <majid.jaidi@gmail.com>
2020-01-29 16:58:29 +0100

    refactored registration component into single form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Layout from "Layout";
-import "styles.css";
+import "styles.scss";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
