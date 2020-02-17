3d987da87595a8f52c409d8dac0133ffd87a1049
Majid Jaidi <majid.jaidi@gmail.com>
2020-02-17 07:54:19 +0100

    added changed compose of middleware

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -10,9 +10,8 @@ if (!isProd) {
   middlewareList.push(logger);
 }
 
-const middleware = compose(
-  applyMiddleware(...middlewareList),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = composeEnhancers(applyMiddleware(...middlewareList));
 
 export default middleware;
